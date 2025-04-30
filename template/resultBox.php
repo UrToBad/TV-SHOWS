77d97594ee6bf0a62a4c05f1a726e5172ba64bde
AdrienVin <121306851+AdrienVin@users.noreply.github.com>
2025-04-30 16:51:00 +0200

    index2 test

--- template/resultBox.php
+++ template/resultBox.php
@@ -4,9 +4,9 @@ class resultBox
     public static function render(string $title, string $tags): void
     { ?>
         <div class="result_box">
-            <div class="result_box_title">Title</div>
+            <div class="result_box_title"><?php $title?></div>
             <div class="result_box_content"></div>
-            <div class="result_box_tags">Tags</div>
+            <div class="result_box_tags"><?php $tags?></div>
         </div>
         <?php
     }

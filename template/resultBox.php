b48291ec0b96dc0d61c347d87abc631b673ab3c5
AdrienVin <121306851+AdrienVin@users.noreply.github.com>
2025-04-30 16:55:05 +0200

    prout

--- template/resultBox.php
+++ template/resultBox.php
@@ -1,12 +1,16 @@
 <?php
 class resultBox
 {
-    public static function render(string $title, array $tags): void
+    public static function render(string $title, array $tags): void {
+    $tagprint = "";
+    foreach ($tags as $tag) {
+        tagprint . tag;
+    }
     { ?>
         <div class="result_box">
-            <div class="result_box_title"><?php $title?></div>
+            <div class="result_box_title"><?php echo $title?></div>
             <div class="result_box_content"></div>
-            <div class="result_box_tags"><?php $tags?></div>
+            <div class="result_box_tags"><?php echo $tagprint?></div>
         </div>
         <?php
     }

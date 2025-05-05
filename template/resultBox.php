6c94b2fd6b826556d3c39c6d9af06de5b69fa1c3
AdrienVin <121306851+AdrienVin@users.noreply.github.com>
2025-05-05 09:30:05 +0200

    add resultbox background

--- template/resultBox.php
+++ template/resultBox.php
@@ -1,7 +1,7 @@
 <?php
 class resultBox
 {
-    public static function render(?string $title = NULL, ?array $tags = NULL): void
+    public static function render(?string $title = NULL, ?array $tags = NULL, ?string $bg_path = NULL): void
     {
         $tagprint = "";
         if ($tags != NULL) {
@@ -14,7 +14,7 @@ class resultBox
             <link rel="stylesheet" href="style/resultbox.css">
             <div class="result_box">
                 <div class="result_box_title"><?php echo $title?></div>
-                <div class="result_box_content"></div>
+                <div class="result_box_content style="background-image: url(' <?php $bg_path ?> ');""></div>
                 <div class="result_box_tags"><?php echo $tagprint?></div>
             </div>
             <?php

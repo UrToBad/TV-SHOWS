31b5c3b6db76f68831b724bdabc306136ec3ba98
AdrienVin <121306851+AdrienVin@users.noreply.github.com>
2025-05-05 15:31:55 +0200

    add button

--- template/Searchbar.php
+++ template/Searchbar.php
@@ -6,6 +6,10 @@ class Searchbar
         <link rel="stylesheet" href="style/searchbar.css">
         <div id="searchbar">
             <div class="search_item"><input type="text" id="search_tag" class="search_input" placeholder="Rechercher..."></div>
+            <?php
+            if (session_status() === PHP_SESSION_NONE) session_start();
+                $addButton = !empty($_SESSION['connecte']) ? "<span class='addButton'>➕</span>" : ""; ?>
+            <div><?php echo $addButton ?></div>
         </div>
         <?php
     }

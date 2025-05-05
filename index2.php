80dd170fed2da3bfddfa622476e758451e0ee7e4
Charles <unconfigured@null.spigotmc.org>
2025-05-05 09:19:15 +0200

    fix

--- index2.php
+++ index2.php
@@ -1,13 +1,27 @@
 <?php
     require_once "template/userContent.php";
     require_once "template/resultBox.php";
+    require_once 'class/SqlCredentials.php';
+    require_once 'class/DatabaseConnection.php';
+    require_once 'controller/SerieController.php';
+
+    $sqlCredentials = new SqlCredentials(
+        "localhost", // Host
+        "3306",
+        "tvshows",   // Database
+        "root",      // Username
+        "root"       // Password
+    );
+
+    $connection = new DatabaseConnection($sqlCredentials);
+    $serieController = new SerieController($connection);
         
     ob_start();
 
     $pagecontent = "";
 
-    for ($i=0; $i < 10 ; $i++) { 
-        $pagecontent . resultBox::render();
+    foreach ($serieController->getAllSeries() as $serie) {
+        $pagecontent . resultBox::render($serie->getTitre(), $serie->getTags());
     }
 
     $pagecontent=ob_get_clean();
--- template/resultBox.php
+++ template/resultBox.php
@@ -1,22 +1,23 @@
 <?php
 class resultBox
 {
-    public static function render(string $title = NULL, array $tags = NULL): void {
-    $tagprint = "";
-    if ($tags != NULL) {
-        foreach ($tags as $tag) {
-        $tagprint . $tag->getNom();
+    public static function render(?string $title = NULL, ?array $tags = NULL): void
+    {
+        $tagprint = "";
+        if ($tags != NULL) {
+            foreach ($tags as $tag) {
+                $tagprint .= htmlspecialchars($tag->getNom()) . ", ";
+            }
         }
-    }
         
-    { ?>
-        <link rel="stylesheet" href="style/resultbox.css">
-        <div class="result_box">
-            <div class="result_box_title"><?php echo $title?></div>
-            <div class="result_box_content"></div>
-            <div class="result_box_tags"><?php echo $tagprint?></div>
-        </div>
-        <?php
+        { ?>
+            <link rel="stylesheet" href="style/resultbox.css">
+            <div class="result_box">
+                <div class="result_box_title"><?php echo $title?></div>
+                <div class="result_box_content"></div>
+                <div class="result_box_tags"><?php echo $tagprint?></div>
+            </div>
+            <?php
         }
     }
 }

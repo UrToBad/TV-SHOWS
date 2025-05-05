747462b4a783a90cfe35ef746d33dc9bff6407ae
AdrienVin <121306851+AdrienVin@users.noreply.github.com>
2025-05-05 15:34:23 +0200

    yes

--- template/Searchbar.php
+++ template/Searchbar.php
@@ -4,13 +4,12 @@ class Searchbar
     public static function render(): void
     { ?>
         <link rel="stylesheet" href="style/searchbar.css">
-        < id="searchbar">
+        <div id="searchbar">
             <div class="search_item"><input type="text" id="search_tag" class="search_input" placeholder="Rechercher..."></div>
             <?php
             if (session_status() === PHP_SESSION_NONE) session_start();
                 $addButton = !empty($_SESSION['connecte']) ? "<span class='addButton'>➕</span>" : ""; ?>
-            <?php echo $addButton ?>
-        </div>
+            <?php echo $addButton ?></div>
         <?php
     }
 }

7e33960ead7a4deaf1e20a1567ea69ac549d6240
AdrienVin <121306851+AdrienVin@users.noreply.github.com>
2025-05-05 13:13:37 +0200

    search improvement

--- template/Searchbar.php
+++ template/Searchbar.php
@@ -5,10 +5,7 @@ class Searchbar
     { ?>
         <link rel="stylesheet" href="style/searchbar.css">
         <div id="searchbar">
-            <div class="search_item">tag<input type="text" id="search_tag" class="search_input"></div>
-            <div class="search_item">serie<input type="text" id="search_ser" class="search_input"></div>
-            <div class="search_item">director<input type="text" id="search_rea" class="search_input"></div>
-            <div class="search_item">actor<input type="text" id="search_act" class="search_input"></div>
+            <div class="search_item">tag<input type="text" id="search_tag" class="search_input" placeholder="Rechercher..."></div>
         </div>
         <?php
     }

f41f6b1f03ea0f18cf4d2a0791b27bc9aed7fbc2
AdrienVin <121306851+AdrienVin@users.noreply.github.com>
2025-05-05 11:40:54 +0200

    resultBox render edit

--- template/ResultBox.php
+++ template/ResultBox.php
@@ -1,7 +1,7 @@
 <?php
 class resultBox
 {
-    public static function render(int $id, ?string $title = NULL, ?array $tags = NULL, ?string $bg_path = NULL, string $type = "series"): void
+    public static function render(int $id, ?string $title = NULL, ?array $tags = NULL, ?string $bg_path = NULL, string $type = "series", ?string $bot_content = NULL): void
     {
         $tagprint = "";
         if ($tags != NULL) {
@@ -15,7 +15,7 @@ class resultBox
             <div class="result_box" data-id="<?php echo $id; ?>" data-type="<?php echo $type; ?>">
                 <div class="result_box_title"><?php echo $title?></div>
                 <div class="result_box_content" style="background-image: url('<?php echo htmlspecialchars($bg_path); ?>');"></div>
-                <div class="result_box_tags"><?php echo $tagprint?></div>
+                <div class="result_box_tags"><?php echo $tagprint, $bot_content?></div>
             </div>
             <?php
         }
